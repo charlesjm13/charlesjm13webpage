e19c929314f179d3a9eaebbff7cf029a869d06a3
Charles Mennerick <charlesjm13@gmail.com>
2021-03-28 19:16:07 -0500

    Added tabs

---
+++ src/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="home">
+    <b-tabs title="Home" active>
+      <p>Welcome to my webpage</p>
+    </b-tabs>
+    <b-tabs title="About Me" active>
+      <p>My name is Charles</p>
+    </b-tabs>
+    <b-tabs title="My Resume" active>
+      <img alt="Vue logo" src="./assets/CharlesMennerickResume.png">
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+
+}
+</script>
+
+<style>
+#home {
+ margin: auto; 
+ text-align: center;
+}
+</style>
